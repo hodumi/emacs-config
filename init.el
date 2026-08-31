fedc46841f032c541008c4c7b1a9d8ce3d9f830c
y.kudo <y.kudo@hoc-net.jp>
2026-08-31 09:42:32 +0900

    add func

--- init.el
+++ init.el
@@ -693,6 +693,21 @@
 
     )
 
+
+  ;; delete word
+  (defun delete-word (arg)
+    "Delete characters forward until encountering the end of a word.
+With argument, do this that many times."
+    (interactive "p")
+    (delete-region (point) (progn (forward-word arg) (point))))
+
+  (defun backward-delete-word (arg)
+    "Delete characters backward until encountering the end of a word.
+With argument, do this that many times."
+    (interactive "p")
+    (delete-word (- arg)))
+
+
   ;; fix-separate
   (defun fix-separate (str lst)
     (cond
